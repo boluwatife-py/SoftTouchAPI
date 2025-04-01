48b51bf35dd5ef33a739d78aee4a33f75d82f203
Boluwatife <akingbadeboluwatife030@gmail.com>
2025-04-01 09:01:12 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-python -m spacy download en_core_web_sm
 pip3 install git+https://github.com/openai/whisper.git werkzeug
 pip install -r requirements.txt
+python -m spacy download en_core_web_sm

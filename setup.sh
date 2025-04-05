14a1ea6fb8c71d41c38dc4c32f30cb73c9f1370c
SoftTouch <boluwatife8081@gmail.com>
2025-04-05 09:58:44 +0000

    updated url

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 pip cache purge
-#pip3 install git+https://github.com/openai/whisper.git werkzeug
+pip3 install git+https://github.com/openai/whisper.git werkzeug
 pip install -r requirements.txt
 python -m spacy download en_core_web_sm
